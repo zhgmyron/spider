9422993b4bbf13a8a84a5f3974bcb2ce465d8b99
zhaoR <guangking1987@163.com>
2016-11-23 16:35:03 +0800

    update

--- zhihu.py
+++ zhihu.py
@@ -1,19 +1,15 @@
-__author__ = 'zhaor'
 #-*-coding=utf-8
-import urllib
-import urllib2
-url ='https://www.zhihu.com/#signin'
+import requests
+url1 ='http://ron.dev.revo.pricerunner.com/'
+url2 ='http://ron.dev.revo.pricerunner.com/merchant/profile/dashboard-pr.php'
+
 user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64)'
-values = {'username' : 'guangking1987@163.com',  'password' : 'Zxcvb1234567' }
+values = {'username' : 'dev_test@email.com',  'password' : 'test' }
 headers = { 'User-Agent' : user_agent }
-data = urllib.urlencode(values)
-request = urllib2.Request(url, data, headers)
-request = urllib2.Request(url)
-try:
-    response = urllib2.urlopen(request)
-except urllib2.URLError, e:
-    print e.reason
+s= requests.session()
+request = s.post(url1, data=values,headers=headers)
 
-page = response.read()
 
-print page.decode('utf-8')
+page = s.get(url2,cookies=requests.cookies,headers=headers)
+
+print (page.text)
